02cf2e5bc5903f1302c23d977c2c9240a21fa577
uttam sharma <sharmauttam8279@gmail.com>
2025-03-31 21:35:58 +0530

    160 days dsa problems and solutions

---
+++ 160 days dsa/ARRAYS/day_1.java
@@ -0,0 +1,38 @@
+// Given an array of positive integers arr[] of size n, the task is to find second-largest distinct element in the array.
+
+//solution---
+// Java program to find the second-largest element in the array
+// using two traversals
+
+// Removed unused import statement
+
+
+
+    // Function to find the second-largest element in the array
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+
+        int largest = -1, secondLargest = -1;
+
+        // Finding the largest element
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+        }
+
+        // Finding the second-largest element
+        for (int i = 0; i < n; i++) {
+            
+            // Update second largest if the current element is greater
+            // than second largest and not equal to the largest
+            if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        System.out.println(getSecondLargest(arr));
+    }
